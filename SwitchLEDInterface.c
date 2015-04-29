6669dfa8c209b385e0205899828309687178560a
GNU generation <eric@asciolla.com>
2015-04-29 11:51:15 -0400

    Still busy.

--- SwitchLEDInterface.c
+++ SwitchLEDInterface.c
@@ -20,24 +20,38 @@ void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 
 // ***** 3. Subroutines Section *****
-
-// PE0, PB0, or PA2 connected to positive logic momentary switch using 10k ohm pull down resistor
-// PE1, PB1, or PA3 connected to positive logic LED through 470 ohm current limiting resistor
-// To avoid damaging your hardware, ensure that your circuits match the schematic
-// shown in Lab8_artist.sch (PCB Artist schematic file) or 
-// Lab8_artist.pdf (compatible with many various readers like Adobe Acrobat).
-int main(void){ unsigned long volatile delay; 
-//**********************************************************************
-// The following version tests input on PE0 and output on PE1
-//**********************************************************************
-  TExaS_Init(SW_PIN_PE0, LED_PIN_PE1);  // activate grader and set system clock to 80 MHz
+void PortE_Init(void) { unsigned long volatile delay;//eja
 	SYSCTL_RCGC2_R  = SYSCTL_RCGC2_R | SYSCTL_RCGC2_GPIOE; //eja
 	delay = SYSCTL_RCGC2_R; //eja
 	GPIO_PORTE_AMSEL_R = GPIO_PORTE_AMSEL_R & ~0x03; //eja
 	GPIO_PORTE_PCTL_R = GPIO_PORTE_PCTL_R & ~0x000000FF; //eja
 	GPIO_PORTE_DIR_R = GPIO_PORTE_DIR_R | 0x02; //eja
 	GPIO_PORTE_DIR_R = GPIO_PORTE_DIR_R & ~0x01; //eja
-	
+	GPIO_PORTE_PUR_R = 0x00000000; //eja
+	GPIO_PORTE_DATA_R = GPIO_PORTE_DATA_R | 0x08;
+}
+
+void delay100ms(unsigned long time) { //eja
+	unsigned long i;
+	while (time > 0) { //eja
+		i = 1333333; //eja
+		while (i > 0) { //eja
+			i = i - 1; //eja
+		} //eja
+		time = time - 1; //eja
+	} //eja
+} //eja
+// PE0, PB0, or PA2 connected to positive logic momentary switch using 10k ohm pull down resistor
+// PE1, PB1, or PA3 connected to positive logic LED through 470 ohm current limiting resistor
+// To avoid damaging your hardware, ensure that your circuits match the schematic
+// shown in Lab8_artist.sch (PCB Artist schematic file) or 
+// Lab8_artist.pdf (compatible with many various readers like Adobe Acrobat).
+int main(void){
+//**********************************************************************
+// The following version tests input on PE0 and output on PE1
+//**********************************************************************
+  TExaS_Init(SW_PIN_PE0, LED_PIN_PE1);  // activate grader and set system clock to 80 MHz
+	PortE_Init();
 	
   EnableInterrupts();           // enable interrupts for the grader
   while(1){
