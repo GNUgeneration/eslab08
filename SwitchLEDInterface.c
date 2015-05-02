96ebef4dcb74db625a7bbd66b6bfe5d42a3182d5
GNU generation <eric@asciolla.com>
2015-05-02 08:32:50 -0400

    Realized one of my mistakes and fixed it.

--- SwitchLEDInterface.c
+++ SwitchLEDInterface.c
@@ -13,6 +13,22 @@
 #include "tm4c123gh6pm.h"
 
 // ***** 2. Global Declarations Section *****
+unsigned long in; //eja
+
+// My Functions:
+
+// Function 1:
+void delay100ms(unsigned long time) { //eja
+	unsigned long i; //eja
+	while (time > 0) { //eja
+		i = 1333333; //eja
+		while (i > 0) { //eja
+			i = i - 1; //eja
+		} //eja
+		time = time - 1; //eja
+	} //eja
+} //eja
+
 
 // FUNCTION PROTOTYPES: Each subroutine defined
 void DisableInterrupts(void); // Disable interrupts
@@ -28,14 +44,28 @@ void EnableInterrupts(void); // Enable interrupts
 
 
 int main(void) {
+	unsigned long volatile delay; //eja
 //**********************************************************************
 // The following version tests input on PE0 and output on PE1
 //**********************************************************************
-	
-	
   TExaS_Init(SW_PIN_PE0, LED_PIN_PE1);  // activate grader and set system clock to 80 MHz
+	
+	SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOE; //eja
+	delay = SYSCTL_RCGC2_R; //eja
+	GPIO_PORTE_AMSEL_R = GPIO_PORTE_AMSEL_R & ~0x03; //eja
+	GPIO_PORTE_PCTL_R = GPIO_PORTE_PCTL_R & ~0x000000FF; //eja
+	GPIO_PORTE_DIR_R = GPIO_PORTE_DIR_R | 0x02; //eja
+	GPIO_PORTE_DIR_R = GPIO_PORTE_DIR_R & ~0x01; //eja
+	
+	GPIO_PORTE_DEN_R = GPIO_PORTE_DEN_R | 0x0004; //eja
+	GPIO_PORTE_DEN_R = GPIO_PORTE_DEN_R | 0x0008; //eja
+	
+	GPIO_PORTE_DATA_R = GPIO_PORTE_DATA_R | 0x0008; //eja
+	
   EnableInterrupts();           // enable interrupts for the grader
   while(1){
 		
   }
 }
+
+
